3347acc781e66ad18b2f8a7d6c330d11c9c31760
Pavel Isayenko <pavel.isayenko@gmail.com>
2020-02-11 00:20:52 +0600

    load_data.py: code split into functions, multiple file processing

--- load_data.py
+++ load_data.py
@@ -2,46 +2,50 @@ import sqlite3
 import json
 from sys import argv
 
+def create_db(sqlitefilename):
+	connection = sqlite3.connect(sqlitefilename)
+	cursor = connection.cursor()
+
+	cursor.execute("""CREATE TABLE IF NOT EXISTS stations
+		(id INTEGER PRIMARY KEY, 
+		name TEXT, 
+		lat REAL, 
+		lon REAL)""")
+	cursor.execute("""CREATE TABLE IF NOT EXISTS pollutants
+		(id INTEGER PRIMARY KEY, 
+		name TEXT)""")
+	cursor.execute("""CREATE TABLE IF NOT EXISTS data
+		(id INTEGER PRIMARY KEY, 
+		pollutant_id INTEGER, 
+		station_id INTEGER, 
+		date TEXT, 
+		value REAL,
+		FOREIGN KEY (pollutant_id)
+		REFERENCES pollutants(id),
+		FOREIGN KEY (station_id)
+		REFERENCES stations(id)
+		)""")
+
+def read_file(filename):
+	print(filename)
+	try:
+		with open(filename, "r") as f:
+			data = json.load(f)
+			for pollutant in data:
+				try:
+					for dates in pollutant['data']:
+						print(pollutant['keyword'], dates['date'], dates['value'])
+				except Exception as e:
+					pass
+	except Exception as e:
+		print("File", filename, "is not accessible")
+
+
 sqlitefilename = "pollution-data.db"
 
 if len(argv) > 1:
-	filename = argv[1]
+	for filename in argv[1:]:
+		read_file(filename)
 else:
-	print("Usage:", __file__, "data_file_name")
-	exit()
+	print("Usage:", __file__, "data_files")
 
-
-connection = sqlite3.connect(sqlitefilename)
-cursor = connection.cursor()
-
-cursor.execute("""CREATE TABLE IF NOT EXISTS stations
-	(id INTEGER PRIMARY KEY, 
-	name TEXT, 
-	lat REAL, 
-	lon REAL)""")
-cursor.execute("""CREATE TABLE IF NOT EXISTS pollutants
-	(id INTEGER PRIMARY KEY, 
-	name TEXT)""")
-cursor.execute("""CREATE TABLE IF NOT EXISTS data
-	(id INTEGER PRIMARY KEY, 
-	pollutant_id INTEGER, 
-	station_id INTEGER, 
-	date TEXT, 
-	value REAL,
-	FOREIGN KEY (pollutant_id)
-	REFERENCES pollutants(id),
-	FOREIGN KEY (station_id)
-	REFERENCES stations(id)
-	)""")
-
-try:
-	with open(filename, "r") as f:
-		data = json.load(f)
-		for pollutant in data:
-			try:
-				for dates in pollutant['data']:
-					print(pollutant['keyword'], dates['date'], dates['value'])
-			except Exception as e:
-				pass
-except Exception as e:
-	print("File", filename, "not accessible")
