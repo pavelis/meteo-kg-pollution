81ba7893d57fd74a6998d9ceee9e8f21322cb7e6
Pavel Isayenko <pavel.isayenko@gmail.com>
2020-02-11 00:56:11 +0600

    PEP8 compliance

--- load_data.py
+++ load_data.py
@@ -1,51 +1,54 @@
 import sqlite3
 import json
 from sys import argv
+import os.path
+
 
 def create_db(sqlitefilename):
-	connection = sqlite3.connect(sqlitefilename)
-	cursor = connection.cursor()
+    connection = sqlite3.connect(sqlitefilename)
+    cursor = connection.cursor()
 
-	cursor.execute("""CREATE TABLE IF NOT EXISTS stations
-		(id INTEGER PRIMARY KEY, 
-		name TEXT, 
-		lat REAL, 
-		lon REAL)""")
-	cursor.execute("""CREATE TABLE IF NOT EXISTS pollutants
-		(id INTEGER PRIMARY KEY, 
-		name TEXT)""")
-	cursor.execute("""CREATE TABLE IF NOT EXISTS data
-		(id INTEGER PRIMARY KEY, 
-		pollutant_id INTEGER, 
-		station_id INTEGER, 
-		date TEXT, 
-		value REAL,
-		FOREIGN KEY (pollutant_id)
-		REFERENCES pollutants(id),
-		FOREIGN KEY (station_id)
-		REFERENCES stations(id)
-		)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS stations
+        (id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        lat REAL, 
+        lon REAL)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS pollutants
+        (id INTEGER PRIMARY KEY, 
+        name TEXT)""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS data
+        (id INTEGER PRIMARY KEY, 
+        pollutant_id INTEGER, 
+        station_id INTEGER, 
+        date TEXT, 
+        value REAL,
+        FOREIGN KEY (pollutant_id)
+        REFERENCES pollutants(id),
+        FOREIGN KEY (station_id)
+        REFERENCES stations(id)
+        )""")
+
 
 def read_file(filename):
-	print(filename)
-	try:
-		with open(filename, "r") as f:
-			data = json.load(f)
-			for pollutant in data:
-				try:
-					for dates in pollutant['data']:
-						print(pollutant['keyword'], dates['date'], dates['value'])
-				except Exception as e:
-					pass
-	except Exception as e:
-		print("File", filename, "is not accessible")
+    print(os.path.basename(filename))
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
+            for pollutant in data:
+                try:
+                    for dates in pollutant['data']:
+                        print(pollutant['keyword'],
+                              dates['date'], dates['value'])
+                except Exception as e:
+                    pass
+    except Exception as e:
+        print("File", filename, "is not accessible")
 
 
 sqlitefilename = "pollution-data.db"
 
 if len(argv) > 1:
-	for filename in argv[1:]:
-		read_file(filename)
+    for filename in argv[1:]:
+        read_file(filename)
 else:
-	print("Usage:", __file__, "data_files")
-
+    print("Usage:", __file__, "data_files")
--- save_pollution_data.py
+++ save_pollution_data.py
@@ -4,8 +4,11 @@ import datetime
 import json
 
 url = "http://gov.meteo.kg/?map=5"
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-response = requests.get(url, headers = headers)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1)'
+    ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+response = requests.get(url, headers=headers)
 response.encoding = 'utf-8'
 
 curdate = datetime.datetime.today().strftime("%Y%m%d")
@@ -13,21 +16,24 @@ filename_prefix = "output/"
 
 station_id = 0
 try:
-	with open(filename_prefix + "stations_" + curdate + ".txt", "w") as stations_file:
-		for lines in response.text.splitlines():
-			linea = lines.lstrip()
-			if re.match("(var content \= \[)", linea):
-				with open(filename_prefix + "data_" + curdate + "_" + str(station_id) + ".txt", "w") as f:
-					result = re.search("\[", linea)
-					(pos, length) = result.span()
-					linea = linea[pos:-1]
-					f.write(linea + '\n')
-					station_id += 1
-			
-			if re.match("wicon", lines):
-				lineb = lines.replace("wicon(", "{").replace(", content);", "}")
-				stations_file.write(lineb + '\n')
+    with open(filename_prefix + "stations_" + curdate + ".txt", "w") \
+            as stations_file:
+        for lines in response.text.splitlines():
+            linea = lines.lstrip()
+            if re.match("(var content \= \[)", linea):
+                with open(filename_prefix + "data_" + curdate + "_" +
+                          str(station_id) + ".txt", "w") as f:
+                    result = re.search("\[", linea)
+                    (pos, length) = result.span()
+                    linea = linea[pos:-1]
+                    f.write(linea + '\n')
+                    station_id += 1
+
+            if re.match("wicon", lines):
+                lineb = lines.replace(
+                    "wicon(", "{").replace(", content);", "}")
+                stations_file.write(lineb + '\n')
 except Exception as e:
-	raise
+    raise
 
 print(station_id, "stations saved")
